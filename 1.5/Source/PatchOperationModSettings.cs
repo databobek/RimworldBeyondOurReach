f83206f1968029f3e7e06fb888ad6a314bb45d8b
Thekiborg <none>
2025-03-02 20:05:41 +0100

    Added dynamic mod settings

---
+++ 1.5/Source/PatchOperationModSettings.cs
@@ -0,0 +1,34 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using Verse;
+
+namespace BeyondOurReachModSettings
+{
+#pragma warning disable CS0649
+	public class PatchOperationModSettings : PatchOperationPathed
+	{
+		private PatchOperation match;
+		private PatchOperation nomatch;
+		private ModSettingDef modSettingDef;
+
+		protected override bool ApplyWorker(XmlDocument xml)
+		{
+			var contentEnabled = BeyondOurReachModSettings.SettingsDict.TryGetValue(modSettingDef, true);
+
+			if (contentEnabled)
+			{
+				if (match != null)
+				{
+					return match.Apply(xml);
+				}
+			}
+			else if (nomatch != null)
+			{
+				return nomatch.Apply(xml);
+			}
+			return true;
+		}
+	}
+}
