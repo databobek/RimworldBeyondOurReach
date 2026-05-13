56ca098634dddbb4e334d555a23736fb4452b4bd
Thekiborg <doflr415@gmail.com>
2026-05-13 20:21:18 +0200

    Hardcoded workgivers? I barely know her

---
+++ 1.6/Mods/ChemfuelPoweredDeepdrill/Source/Defofs.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+
+namespace BORChemfuelPoweredDeepdrill
+{
+	[DefOf]
+	public static class Defofs
+	{
+		static Defofs()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(Defofs));
+		}
+
+		public static ThingDef BOR_CPD_FuelRodDeepdrill;
+		public static ThingDef BOR_CPD_QuasarNeutroniumDeepdrill;
+	}
+}
---
+++ 1.6/Mods/ChemfuelPoweredDeepdrill/Source/Workgiver_QuasarNeutroniumDeepdrill.cs
@@ -0,0 +1,9 @@
+﻿using Verse;
+
+namespace BORChemfuelPoweredDeepdrill
+{
+	public class Workgiver_QuasarNeutroniumDeepdrill : ChemfuelGeneratorDeepdrill.WorkGiver_ChemfuelGeneratorDeepDrill
+	{
+		public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForDef(Defofs.BOR_CPD_QuasarNeutroniumDeepdrill);
+	}
+}
