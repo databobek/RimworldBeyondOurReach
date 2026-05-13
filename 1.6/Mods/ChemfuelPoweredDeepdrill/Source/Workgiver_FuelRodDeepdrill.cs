fcfda2f86a3c0467328c361070aef09d755c9cb7
Thekiborg <doflr415@gmail.com>
2026-05-13 21:41:25 +0200

    Fix

--- 1.6/Mods/ChemfuelPoweredDeepdrill/Source/Workgiver_FuelRodDeepdrill.cs
+++ 1.6/Mods/ChemfuelPoweredDeepdrill/Source/Workgiver_FuelRodDeepdrill.cs
@@ -1,9 +1,30 @@
-﻿using Verse;
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
 
 namespace BORChemfuelPoweredDeepdrill
 {
 	public class Workgiver_FuelRodDeepdrill : ChemfuelGeneratorDeepdrill.WorkGiver_ChemfuelGeneratorDeepDrill
 	{
 		public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForDef(Defofs.BOR_CPD_FuelRodDeepdrill);
+
+
+		public override bool ShouldSkip(Pawn pawn, bool forced = false)
+		{
+			List<Building> allBuildingsColonist = pawn.Map.listerBuildings.allBuildingsColonist;
+			for (int i = 0; i < allBuildingsColonist.Count; i++)
+			{
+				Building building = allBuildingsColonist[i];
+				if (building.def == Defofs.BOR_CPD_FuelRodDeepdrill)
+				{
+					CompPowerTrader comp = building.GetComp<CompPowerPlant>();
+					if ((comp == null || comp.PowerOutput > 0) && building.Map.designationManager.DesignationOn(building, DesignationDefOf.Uninstall) == null)
+					{
+						return false;
+					}
+				}
+			}
+			return true;
+		}
 	}
 }
--- 1.6/Mods/ChemfuelPoweredDeepdrill/Source/Workgiver_QuasarNeutroniumDeepdrill.cs
+++ 1.6/Mods/ChemfuelPoweredDeepdrill/Source/Workgiver_QuasarNeutroniumDeepdrill.cs
@@ -1,9 +1,29 @@
-﻿using Verse;
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
 
 namespace BORChemfuelPoweredDeepdrill
 {
 	public class Workgiver_QuasarNeutroniumDeepdrill : ChemfuelGeneratorDeepdrill.WorkGiver_ChemfuelGeneratorDeepDrill
 	{
 		public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForDef(Defofs.BOR_CPD_QuasarNeutroniumDeepdrill);
+
+		public override bool ShouldSkip(Pawn pawn, bool forced = false)
+		{
+			List<Building> allBuildingsColonist = pawn.Map.listerBuildings.allBuildingsColonist;
+			for (int i = 0; i < allBuildingsColonist.Count; i++)
+			{
+				Building building = allBuildingsColonist[i];
+				if (building.def == Defofs.BOR_CPD_QuasarNeutroniumDeepdrill)
+				{
+					CompPowerTrader comp = building.GetComp<CompPowerPlant>();
+					if ((comp == null || comp.PowerOutput > 0) && building.Map.designationManager.DesignationOn(building, DesignationDefOf.Uninstall) == null)
+					{
+						return false;
+					}
+				}
+			}
+			return true;
+		}
 	}
 }
