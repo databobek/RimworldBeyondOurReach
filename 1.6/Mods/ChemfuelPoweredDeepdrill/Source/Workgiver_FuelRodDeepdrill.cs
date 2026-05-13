b058a259e3ce136fafaedc332d9e0c7dc9f61558
Thekiborg <doflr415@gmail.com>
2026-05-13 20:21:18 +0200

    Hardcoded workgivers? I barely know her

---
+++ 1.6/Mods/ChemfuelPoweredDeepdrill/Source/Workgiver_FuelRodDeepdrill.cs
@@ -0,0 +1,9 @@
+﻿using Verse;
+
+namespace BORChemfuelPoweredDeepdrill
+{
+	public class Workgiver_FuelRodDeepdrill : ChemfuelGeneratorDeepdrill.WorkGiver_ChemfuelGeneratorDeepDrill
+	{
+		public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForDef(Defofs.BOR_CPD_FuelRodDeepdrill);
+	}
+}
